9f8f91e684d50cd31aa451a9723bf488bfdd9832
Bharat Bhushan Verma <dearbharat@gmail.com>
2021-06-08 08:28:36 -0300

    Shiny

---
+++ Shiny_Covid_Speeches.R
@@ -0,0 +1,69 @@
+# deployed app: https://minecr.shinyapps.io/fm-speeches-covid19-simple/
+# load packages ----------------------------------------------------------------
+
+library(shiny)
+library(tidyverse)
+library(tidytext)
+library(glue)
+library(plotly)
+
+# read data --------------------------------------------------------------------
+
+covid_speeches_words <- read_rds("covid-speeches-words.rds")
+
+# ui ---------------------------------------------------------------------------
+
+ui <- fluidPage(
+  titlePanel("Scotland and UK COVID-19 Speeches"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "origin",
+        label   = "Select origin",
+        choices = c("Scotland", "UK")
+      ),
+      dateRangeInput(
+        inputId = "date_range",
+        label   = "Select date range",
+        start   = min(covid_speeches_words$date),
+        end     = max(covid_speeches_words$date)
+      )
+    ),
+    mainPanel(
+      plotlyOutput(outputId = "sentiment_plot")
+    )
+  )
+)
+
+# server -----------------------------------------------------------------------
+
+server <- function(input, output) {
+  output$sentiment_plot <- renderPlotly({
+    p <- covid_speeches_words %>%
+      filter(origin == input$origin) %>%
+      filter(between(date, input$date_range[1], input$date_range[2])) %>%
+      anti_join(stop_words) %>%
+      filter(word != "positive") %>%
+      inner_join(get_sentiments("bing"), by = "word") %>%
+      count(date, sentiment) %>%
+      pivot_wider(names_from = sentiment, values_from = n) %>%
+      mutate(sentiment = positive - negative) %>%
+      ggplot(aes(x = date, y = sentiment)) +
+      geom_line(color = "gray") +
+      geom_point(aes(color = sentiment > 0), size = 2) +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "lightgray") +
+      labs(
+        title = glue("Daily sentiment score, {input$origin} COVID-19 briefings"),
+        x = "Date", y = "Sentiment score (positive - negative)"
+      ) +
+      theme_minimal() +
+      theme(legend.position = "none")
+    
+    ggplotly(p, tooltip = c("x", "y"))
+  })
+}
+
+# create shiny app -------------------------------------------------------------
+
+shinyApp(ui = ui, server = server)
---
+++ covid-speeches-uk.R
@@ -0,0 +1,66 @@
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(tidytext)
+library(lubridate)
+library(here)
+
+# read data --------------------------------------------------------------------
+
+covid_speeches_uk <- read_rds("covid-speeches-uk.rds") %>%
+  rowid_to_column(var = "speech_no")
+
+# new columns ------------------------------------------------------------------
+
+covid_speeches_uk <- covid_speeches_uk %>%
+  mutate(
+    # name of day
+    wday = wday(date, label = TRUE),
+    # weekend/weekday
+    weekend = if_else(wday %in% c("Sat", "Sun"), "Weekend", "Weekday")
+  ) %>%
+  # count words
+  rowwise() %>%
+  mutate(n_words = text %>% str_count("\\w+") %>% sum()) %>%
+  ungroup()
+
+# tokenize words ---------------------------------------------------------------
+
+covid_speeches_uk_words <- covid_speeches_uk %>%
+  # make sure COVID-19 (and all its various spellings) don't get split
+  # tidytext doesn't remove underscores
+  # https://stackoverflow.com/questions/58281091/preserve-hyphenated-words-in-ngrams-analysis-with-tidytext
+  mutate(
+    text = str_replace_all(text, "COVID-19", "COVID_19"),
+    text = str_replace_all(text, "COVID 19", "COVID_19"),
+    text = str_replace_all(text, "Covid-19", "COVID_19"),
+    text = str_replace_all(text, "Covid 19", "COVID_19")
+  ) %>%
+  unnest_tokens(word, text)
+
+# tokenize 2-grams -------------------------------------------------------------
+
+covid_speeches_uk_bigrams <- covid_speeches_uk %>%
+  # make sure COVID-19 (and all its various spellings) don't get split
+  # tidytext doesn't remove underscores
+  # https://stackoverflow.com/questions/58281091/preserve-hyphenated-words-in-ngrams-analysis-with-tidytext
+  mutate(
+    text = str_replace_all(text, "COVID-19", "COVID_19"),
+    text = str_replace_all(text, "COVID 19", "COVID_19"),
+    text = str_replace_all(text, "Covid-19", "COVID_19"),
+    text = str_replace_all(text, "Covid 19", "COVID_19")
+  ) %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  # drop bigrams with stopwords
+  mutate(i = row_number()) %>%    # add index for later grouping
+  unnest_tokens(word, bigram, drop = FALSE) %>%    # tokenize bigrams into words
+  anti_join(stop_words) %>%    # drop rows with stop words
+  group_by(i) %>%    # group by bigram index
+  filter(n() == 2) %>%    # drop bigram instances where only one word left
+  summarise(bigram = unique(bigram), .groups = "drop")
+
+# save data --------------------------------------------------------------------
+
+write_rds(covid_speeches_uk, file = "covid-speeches-uk.rds", compress = "xz")
+write_rds(covid_speeches_uk_bigrams, file = "covid-speeches-uk-bigrams.rds", compress = "xz")
+write_rds(covid_speeches_uk_words, file = "covid-speeches-uk-words.rds", compress = "xz")
